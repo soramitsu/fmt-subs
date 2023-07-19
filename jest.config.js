337349dea05114f6b489bca093f2de608b5517dc
C4tWithShell <shelkovnikov@soramitsu.co.jp>
2023-07-19 14:46:17 +0300

    Set up sonar,dojo

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,8 @@ module.exports = {
     '^.+\\.ts$': ['esbuild-jest', { sourcemap: true, target: 'es2020' }],
   },
   testMatch: ['**/src/**/*.spec.ts', '**/src/**/__tests__/**/*.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageReporters: ['lcov'],
+  coveragePathIgnorePatterns: ['node_modules/', 'coverage/'],
 }
